9dd19b933bf3febbf505565a7e3af4573bec017c
BuildTools <jerskisnow@gmail.com>
2021-11-12 19:11:21 +0100

    w

--- commands/config.js
+++ commands/config.js
@@ -85,7 +85,7 @@ module.exports.buttons = [
 
             // Edit the message
             await interaction.message.edit({ embeds: [embed], components: [row] })
-            interaction.deferUpdate()
+            interaction.deferUpdate().catch(console.error)
         }
     },
     {
@@ -106,7 +106,7 @@ module.exports.buttons = [
                 .setColor(config.embedColor.b)
                 .setDescription('In which channel should the suggestions show up? (Type: #channel)')
 
-            interaction.message.edit({ embeds: [embed], rows: [] })
+            interaction.message.edit({ embeds: [embed], rows: null })
 
             const filter = msg => msg.author.id === interaction.user.id
 
@@ -131,7 +131,7 @@ module.exports.buttons = [
             embed.setDescription(`Suggestions will now show up in <#${chnId}>.`)
 
             await interaction.message.edit({ embeds: [embed] })
-            interaction.deferReply()
+            interaction.deferReply().catch(console.error)
         }
     },
     {
@@ -152,7 +152,7 @@ module.exports.buttons = [
                 .setColor(config.embedColor.b)
                 .setDescription('In which channel should the reports show up? (Type: #channel)')
 
-            interaction.message.edit({ embeds: [embed], rows: [] })
+            interaction.message.edit({ embeds: [embed], rows: null })
 
             const filter = msg => msg.author.id === interaction.user.id
 
@@ -177,7 +177,7 @@ module.exports.buttons = [
             embed.setDescription(`Reports will now show up in <#${chnId}>.`)
 
             await interaction.message.edit({ embeds: [embed] })
-            interaction.deferReply()
+            interaction.deferReply().catch(console.error)
         }
     },
 
@@ -211,7 +211,7 @@ module.exports.buttons = [
 
             // Edit the message
             await interaction.message.edit({ embeds: [embed], components: [row] })
-            await interaction.deferUpdate()
+            await interaction.deferUpdate().catch(console.error)
         }
     },
     {
@@ -232,7 +232,7 @@ module.exports.buttons = [
                 .setColor(config.embedColor.b)
                 .setDescription('Which role should be able to interact with created Suggestions & Reports? (Type: @role)')
 
-            interaction.message.edit({ embeds: [embed], rows: [] })
+            interaction.message.edit({ embeds: [embed], rows: null })
 
             const filter = msg => msg.author.id === interaction.user.id
 
@@ -272,7 +272,7 @@ module.exports.buttons = [
             embed.setDescription(`The ${role.name} can now interact with Suggestions & Reports.`)
 
             await interaction.message.edit({ embeds: [embed] })
-            interaction.deferReply()
+            interaction.deferReply().catch(console.error)
         }
     },
 
@@ -298,7 +298,7 @@ module.exports.buttons = [
 
             // Edit the message
             await interaction.message.edit({ embeds: [embed] })
-            interaction.deferUpdate()
+            interaction.deferUpdate().catch(console.error)
         }
     }
 ]
