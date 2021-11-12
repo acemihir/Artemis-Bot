3b1d550fab3276f379713cbd58ae9d3c0442c1a5
BuildTools <jerskisnow@gmail.com>
2021-11-12 16:44:24 +0100

    hmm

--- index.js
+++ index.js
@@ -2,49 +2,49 @@ const { ShardingManager } = require('discord.js')
 const config = require('./config')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-const manager = new ShardingManager('app.js', { token: config.botToken })
-
 const main = async function () {
+    const manager = new ShardingManager('app.js', { token: config.botToken })
+    
+    // Spawn the shard
+    manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
+    await manager.spawn()
+
     // Production stuff
-    if (!config.devMode) {
-        // Every 30 minutes
-        setInterval(async () => {
-            // TopGG (top.gg)
-            const guildCount = (await manager.fetchClientValues('guilds.cache.size')).reduce((a, b) => a + b, 0)
+    // if (!config.devMode) {
+    //     // Every 30 minutes
+    //     setInterval(async () => {
+    //         // TopGG (top.gg)
+    //         const guildCount = (await manager.fetchClientValues('guilds.cache.size')).reduce((a, b) => a + b, 0)
 
-            await fetch(`https://top.gg/api/bots/${config.botId}/stats`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    server_count: guildCount,
-                    shard_count: manager.totalShards,
-                })
-            })
+    //         await fetch(`https://top.gg/api/bots/${config.botId}/stats`, {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 server_count: guildCount,
+    //                 shard_count: manager.totalShards,
+    //             })
+    //         })
 
-            console.log(`Posted stats to TopGG (server_count: ${guildCount}, shard_count: ${manager.totalShards})`)
+    //         console.log(`Posted stats to TopGG (server_count: ${guildCount}, shard_count: ${manager.totalShards})`)
 
-            // BotsForDiscord (discords.com/bots)
-            await fetch(`https://discords.com/bots/api/bot/${config.botId}`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    server_count: guildCount
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': config.apis.discordsToken
-                }
-            })
+    //         // BotsForDiscord (discords.com/bots)
+    //         await fetch(`https://discords.com/bots/api/bot/${config.botId}`, {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 server_count: guildCount
+    //             }),
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //                 'Authorization': config.apis.discordsToken
+    //             }
+    //         })
 
-            // Discord Bots (discord.bots.gg)
-            // TODO: This
+    //         // Discord Bots (discord.bots.gg)
+    //         // TODO: This
 
-            // Discord Bot List (discordbotlist.com)
-            // TODO: This
-        }, 1800000)
-    }
+    //         // Discord Bot List (discordbotlist.com)
+    //         // TODO: This
+    //     }, 1800000)
+    // }
 }
 
 main()
-
-// Spawn the shard
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
-manager.spawn(6, 15000, false)
