d86e6ee5110fb1addc1984bdb838e6d554b38f99
BuildTools <jerskisnow@gmail.com>
2021-11-12 18:08:41 +0100

    status

--- listeners/ready.js
+++ listeners/ready.js
@@ -4,14 +4,14 @@ const config = require('../config')
 // ================================
 module.exports = async function (client) {
     // Set an interval for the activity so all guilds are loaded/cached before counting.
-    setInterval(async function () {
-        const guilds_result = await client.shard.fetchClientValues('guilds.cache.size')
-        const guildCount = guilds_result.reduce((prev, count) => prev + count, 0)
+    // setInterval(async function () {
+    //     const guilds_result = await client.shard.fetchClientValues('guilds.cache.size')
+    //     const guildCount = guilds_result.reduce((prev, count) => prev + count, 0)
 
-        client.user.setActivity(`${guildCount} servers | ${client.shard.count} shards`, {
-            type: 'WATCHING'
-        })
-    }, config.activityUpdateInterval * 60 * 1000)
+    //     client.user.setActivity(`${guildCount} servers | ${client.shard.count} shards`, {
+    //         type: 'WATCHING'
+    //     })
+    // }, config.activityUpdateInterval * 60 * 1000)
 
     console.log('Fully started.')
 }
